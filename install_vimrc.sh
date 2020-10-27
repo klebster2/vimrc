e25245f092e4631e71c978bfd674829629dc69df
klebster2 <42589399+klebster2@users.noreply.github.com>
2020-10-27 21:43:20 +0000

    add basic vimrc and fix installation script

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -4,7 +4,7 @@ set -e
 
 repo_dir="`dirname \"$0\"`"
 
-cd "~/.vim_runtime"
+#pushd $(realpath "~/.vim_runtime")
 
 echo 'set runtimepath+=~/.vim_runtime
 source ~/.vim_runtime/vimrcs/basic.vim
@@ -17,5 +17,5 @@ source ~/.vim_runtime/vimrcs/basic.vim
 "catch
 "endtry' > ~/.vimrc
 
-echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
+echo "Installed klebster2's Vim configuration successfully."
 
