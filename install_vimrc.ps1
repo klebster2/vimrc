8e8e746190cbec99f66e11b67278550a1320be3c
klebster2 <42589399+klebster2@users.noreply.github.com>
2021-02-07 23:16:48 +0000

    fix: powershell newline

--- install_vimrc.ps1
+++ install_vimrc.ps1
@@ -1,13 +1,9 @@
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni $HOME/vimfiles/autoload/plug.vim -Force
 
-<<<<<<< HEAD
 Remove-Item $HOME\.vimrc
 
 Add-Content $HOME\.vimrc "set runtimepath+=$HOME\.vim_runtime`r`n`r`nsource $HOME\.vim_runtime\vimrcs\plugins.vim"
-=======
-Add-Content $HOME\.vimrc "set runtimepath+=$HOME\.vim_runtime`nsource $HOME\.vim_runtime\vimrcs\plugins.vim`n"
->>>>>>> 7f6becf... add: Windows preferences and powershell
 
 vim +'PlugInstall --sync' +qall
 
