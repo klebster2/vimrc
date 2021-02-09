1736361866e27a3e5f754f421838961ed2591ba1
klebster2 <42589399+klebster2@users.noreply.github.com>
2021-02-09 23:35:06 +0000

    update: basic, readme and install_vimrc

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 set -xe
-
-#if [ -d ~/.vim ]; then
-#    rm -r ~/.vim
-#fi
-
-#mkdir ~/.vim
+echo "Starting vimrc setup..."
 mkdir -p ~/.vim/undodir
 
-echo "Installed vim configuration successfully."
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -24,3 +18,5 @@ vim +PlugInstall +qall
 pushd ~/.vim/plugged/YouCompleteMe
 ./install.py --all
 popd
+
+echo "Installed dependencies for vim configuration successfully."
