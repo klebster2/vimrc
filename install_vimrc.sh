844b11dc9debf0a650188fb1944f831f4a756f82
klebster2 <42589399+klebster2@users.noreply.github.com>
2021-03-20 23:43:12 +0000

    remove garbage

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -10,17 +10,6 @@ if [ ! -d "~/.vim/autoload/plug.vim" ]; then
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-echo "Downloading proverbs from wikiquote"
-
-cat <(curl -vs "https://en.wikiquote.org/wiki/English_proverbs" 2>&1 \
-    | xpath -q -e '//div[@id="mw-content-text"]//*[name()="ul"]//*[(name()="b")]//text()' ./English_proverbs \
-    | grep -Pv "^( |[^A-Z])|([^\.\?\!]$)" ) \
-    <(curl -vs "https://en.wikipedia.org/wiki/List_of_proverbial_phrases" 2>&1 \
-    | xpath -q -e "//div[@class='mw-parser-output']/ul/li/text()" \
-    | grep -Pv "^( |[^A-Z])|[0-9]" ) \
-    | sort -u \
-    > ~/.vim_runtime/proverbs.txt
-
 echo "set runtimepath+=~/.vim_runtime
 source ~/.vim_runtime/vimrcs/plugins.vim
 " > ~/.vimrc
