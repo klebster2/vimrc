c97f8571cead243a99413381fbd28ff8d3447849
klebster2 <42589399+klebster2@users.noreply.github.com>
2022-04-22 22:15:35 +0100

    add init lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,10 @@
---3 types of config opts
---
--- global: vim.o
-require('options')
-require('plugins')
-require('keymappings')
-require('nvim-tree-cfg')
-require('nvim-compe-cfg')
+require("keymappings")
+require("options")
+require("plugins")
+require("nvim-compe-cfg")
+require("nvim-tree-cfg")
 require('lsp')
 require('lsp.lua-ls')
-
---lua require'lspconfig'.pyls.setup{on_attach=require'completion'.on_attach}
-
---vim.g.colors_name = 'gruvbox'
--- empty setup using defaults: add your own options
-
+-- require("./lua/keymappings")
+-- require("./lua/keymappings")
+-- require("./lua/keymappings")
