ea0e842521b4b57a49ffc63fe8eff04373e5f532
klebster2 <42589399+klebster2@users.noreply.github.com>
2023-10-09 16:32:00 +0100

    comment luasnippets

--- nvim/lua/luasnippets.lua
+++ nvim/lua/luasnippets.lua
@@ -4,7 +4,7 @@
 --local fmt = require("luasnip.extras.fmt").fmt
 --local i = ls.insert_node
 --local rep = require("luasnip.extras").rep
---
+---
 --ls.snippets = {
 ---  lua = {
 ---    ls.parser.parse_snippet("lf", "-- Defined in $TM_FILENAME\nlocal $1 = function($2)\n  $0\nend"),
