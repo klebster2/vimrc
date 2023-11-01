2215b05d06bbbc13672a7471f4018749dbd4708b
klebster2 <42589399+klebster2@users.noreply.github.com>
2023-11-01 22:10:48 +0000

    update config and llm plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,27 +1,28 @@
 ---- learn Vimscript the Hard Way: "A trick to learning something is to force yourself to use it." [in other words, remap, and unmap]
 ---- You can return to the previous file by using <c+o> (or <c+i>)
 --- see :help 'runtimepath' or :help 'rtp'
-require("conda-env")              --> $HOME/.vim_runtime/nvim/lua/conda-env.lua  -- Dependencies: (mini)-conda Get CONDA_EXE environment variable
+require("conda-env")                 --> $HOME/.vim_runtime/nvim/lua/conda-env.lua  -- Dependencies: (mini)-conda Get CONDA_EXE environment variable
 --- Basic options
-require("options")                --> $HOME/.vim_runtime/nvim/lua/options.lua
+require("options")                   --> $HOME/.vim_runtime/nvim/lua/options.lua
 --- Basic keymappings
-require("keymappings")            --> $HOME/.vim_runtime/nvim/lua/keymappings.lua
+require("keymappings")               --> $HOME/.vim_runtime/nvim/lua/keymappings.lua
 --- Packer installation and packer packages installation
-require("packer-install")         --> $HOME/.vim_runtime/nvim/lua/packer-install.lua
-require("packer-startup")         --> $HOME/.vim_runtime/nvim/lua/packer-startup.lua
+require("packer-install")            --> $HOME/.vim_runtime/nvim/lua/packer-install.lua
+require("packer-startup")            --> $HOME/.vim_runtime/nvim/lua/packer-startup.lua
 --- Plugin configurations
-require("plugins/nvim-tree-cfg")  --> $HOME/.vim_runtime/nvim/after/lua/plugins/nvim-tree-cfg.lua
-require("plugins/keymappings")    --> $HOME/.vim_runtime/nvim/after/lua/plugins/keymappings.lua
-require("plugins/options")        --> $HOME/.vim_runtime/nvim/after/lua/options.lua
+require("plugins/nvim-tree-cfg")     --> $HOME/.vim_runtime/nvim/after/lua/plugins/nvim-tree-cfg.lua
+require("plugins/keymappings")       --> $HOME/.vim_runtime/nvim/after/lua/plugins/keymappings.lua
+require("plugins/options")           --> $HOME/.vim_runtime/nvim/after/lua/options.lua
 --- Default CMP + LSP 'Mason' (language server protocol) configuration (for python)
-require("luasnip-config")         --> ?
-require("plugins/nvim-cmp-cfg")   --> $HOME/.vim_runtime/nvim/after/lua/plugins/nvim-cmp-cfg.lua
-require("plugins/nvim-telescope") --> $HOME/.vim_runtime/nvim/after/lua/plugins/nvim-telescope.lua
+require("luasnip-config")            --> $HOME/.vim_runtime/nvim/lua/luasnip-config.lua                 # TODO: move to /after
+--require("plugins/llm")               --> $HOME/.vim_runtime/nvim/after/lua/plugins/llm.lua
+require("plugins/nvim-cmp-cfg")      --> $HOME/.vim_runtime/nvim/after/lua/plugins/nvim-cmp-cfg.lua
+require("plugins/nvim-telescope")    --> $HOME/.vim_runtime/nvim/after/lua/plugins/nvim-telescope.lua
+require("plugins/indent-blank-line") --> $HOME/.vim_runtime/nvim/after/lua/plugins/indent-blank-line.lua
 --- Autocmds (per file type)
-require("autocmds")               --> $HOME/.vim_runtime/nvim/lua/autocmds.lua
+require("autocmds")                  --> $HOME/.vim_runtime/nvim/lua/autocmds.lua
 --- (legacy) vimscript files         --> $HOME/.vim_runtime/vimrcs/
 vim.api.nvim_exec([[ source $HOME/.vim_runtime/vimrcs/basic.vim ]], true)
 vim.api.nvim_exec([[ source $HOME/.vim_runtime/vimrcs/customcompleters/fasttext.vim ]], true)
 vim.api.nvim_exec([[ source $HOME/.vim_runtime/vimrcs/customcompleters/maskprediction.vim ]], true)
 vim.api.nvim_exec([[ source $HOME/.vim_runtime/vimrcs/customcompleters/code-generation.vim ]], true)
-vim.g.copilot_enabled = 1
--- nvim/lua/packer-startup.lua
+++ nvim/lua/packer-startup.lua
@@ -147,8 +147,28 @@ require("packer").startup(function()
       'huggingface/llm.nvim',
       config = function()
         require('llm').setup({
-          -- cf Setup
-        })
+          model = "codellama/CodeLlama-34B-hf", --- also see "bigcode/starcoderplus", "bigcode/starcoder"
+          query_params = {
+            max_new_tokens = 100,
+            temperature = 0.2,             --- lower temperature to get more 'appropriate' output
+            top_p = 0.8,                 --- lower top_p to get more suggestions
+            stop_tokens = nil,
+          },
+          fim = {
+          enabled = true,
+          prefix = "<PRE> ",     ---  "<fim_prefix>" for "codellama/CodeLlama-13b-hf", (the space matters)
+          middle = " <MID>",     ---  "<fim_middle>" for "codellama/CodeLlama-13b-hf", (the space matters)
+          suffix = " <SUF>",     ---  "<fim_suffix>" for "codellama/CodeLlama-13b-hf", (the space matters)
+          },
+          context_window = 4096,        --- max number of tokens for the context window (max for "codellama/CodeLlama-13b-hf": 4096, max for "bigcode/starcoder: 8192)
+          tokenizer = {
+            repository = "codellama/CodeLlama-34B-hf",
+          },
+          lsp = {
+            bin_path = vim.api.nvim_call_function("stdpath", { "data" }) .. "/mason/bin/llm-ls",
+          },
+          enable_suggestions_on_startup = false, -- do not enable by default
+      })
       end
     }
 end)
