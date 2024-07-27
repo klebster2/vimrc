d0f5efe11df76d5ca0356014733c37d0628b9440
klebster2 <42589399+klebster2@users.noreply.github.com>
2024-07-27 23:41:20 +0100

    update lualine.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,38 +3,6 @@ return {
     "christoomey/vim-tmux-navigator",
     lazy=true,
   },
-  { 'nvim-lualine/lualine.nvim',
-    event = "VeryLazy",
-    require('lualine').setup {
-      options = {
-        icons_enabled = true,
-        fmt = string.lower,
-        theme  = require('lualine.themes.gruvbox_dark'),
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
-        disabled_filetypes = {
-          statusline = {},
-          winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
-        }
-      },
-      sections = {
-        lualine_a = { "mode"},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
-      },
-    }
-  },
   { "wsdjeg/vim-lua" },
   { "gelguy/wilder.nvim" },
   { "preservim/tagbar" }, -- view python objects
@@ -43,6 +11,9 @@ return {
   { "sheerun/vim-polyglot" },
   {
     "zbirenbaum/copilot.lua",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
     cmd = "Copilot",
     event = "InsertEnter",
     config = function()
--- lua/plugins/ollama.lua
+++ lua/plugins/ollama.lua
@@ -1,3 +1,5 @@
+--- Currently, this is the same as the example on the ollama page.
+--- We will try something a lil more jazzy.
 return {
   "nomnivore/ollama.nvim",
   dependencies = {
@@ -5,4 +7,41 @@ return {
   },
   -- All the user commands added by the plugin
   cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+  keys = {
+    -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+    {
+      "<leader>oo",
+      ":<c-u>lua require('ollama').prompt()<cr>",
+      desc = "ollama prompt",
+      mode = { "n", "v" },
+    },
+    -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+    {
+      "<leader>oG",
+      ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+      desc = "ollama Generate Code",
+      mode = { "n", "v" },
+    },
+  },
+  ---@type Ollama.Config
+  opts = {
+    model = "arcee-ai/arcee-scribe",
+    url = "http://127.0.0.1:11434",
+    serve = {
+      on_start = false,
+      command = "ollama",
+      args = { "serve" },
+      stop_command = "pkill",
+      stop_args = { "-SIGTERM", "ollama" },
+    },
+    -- View the actual default prompts in ./lua/ollama/prompts.lua
+    prompts = {
+      Sample_Prompt = {
+        prompt = "Show me the money.",
+        input_label = "> ",
+        model = "arcee-ai/arcee-scribe",
+        action = "display",
+      }
+    }
+  }
 }
